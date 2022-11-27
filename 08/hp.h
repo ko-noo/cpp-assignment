f0f20b5bfa217c785e18bccb4568fc2c61f64977
Hyeon-Woo Ko <116580966+ko-noo@users.noreply.github.com>
2022-11-27 13:07:36 +0900

    Create hp.h

---
+++ 08/hp.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+extern int player_hp_num;
+extern int mon_hp_num;
+
+void showhp() {
+	cout << "[HP of player]" << endl;
+	for (int i = player_hp_num; i > 0; i--) {
+		cout << "* ";
+	}
+	cout << endl;
+
+	cout << "[HP of monster]" << endl;
+	for (int i = mon_hp_num; i > 0; i--) {
+		cout << "* ";
+	}
+	cout << endl;
+}
