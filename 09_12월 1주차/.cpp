9d782fe6206956cddb65157502aa18da5a5632ab
Hyeon-Woo Ko <116580966+ko-noo@users.noreply.github.com>
2022-12-03 16:50:05 +0900

    Create .cpp

---
+++ 09_12월 1주차/.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "숫자 개수: ";
+	int n;
+	cin >> n;
+
+	int* vec = new int[n];
+	cout << "숫자 입력" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+	}
+
+	int temp;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - 1; j++) {
+			if (vec[j] > vec[j + 1]) {
+				temp = vec[j];
+				vec[j] = vec[j + 1];
+				vec[j + 1] = temp;
+			}
+		}
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << vec[i] << ' ';
+	}
+}
