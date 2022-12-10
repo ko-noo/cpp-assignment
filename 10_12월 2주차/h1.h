6c0fba6f10ea8510c3ae2a8ff5a862ed0dbe0eee
Hyeon-Woo Ko <116580966+ko-noo@users.noreply.github.com>
2022-12-10 21:26:13 +0900

    Update h1.h

--- 10_12월 2주차/h1.h
+++ 10_12월 2주차/h1.h
@@ -6,18 +6,20 @@ using namespace std;
 class Circle {
 private:
 	int r;
-
+	float S;
 public:
 	Circle(int r) {
 		this->r = r; // 멤버와 인수가 동일할 때
-		cout << "생성자 출현!";
+		S = 3.14 * r * r;
+		cout << "생성자 출현!" << "\n";
 	}
 	
 	~Circle() {
-		cout << "소멸자 출현!";
+		cout << "소멸자 출현!" << "\n";
 	}
 
 	void showArea() {
-		cout << "원의 넓이: " << (r*r*3.14) << endl;
+		
+		cout << "원의 넓이: " << S << endl;
 	}
 };
